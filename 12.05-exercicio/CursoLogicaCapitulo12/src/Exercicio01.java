ad39e5faaae366c1ddbd55b750b397e39c4858b7
AlsGomes <als_08.net@hotmail.com>
2023-02-01 17:51:24 -0300

    Ajuste resolução

--- 12.05-exercicio/CursoLogicaCapitulo12/src/Exercicio01.java
+++ 12.05-exercicio/CursoLogicaCapitulo12/src/Exercicio01.java
@@ -2,57 +2,38 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
 
 public class Exercicio01 {
 
 	public static void main(String[] args) throws IOException, EmailException {
 		Path arquivo = Paths.get("/tmp/nomes.txt");
-		
+
 		List<String> nomes = Files.readAllLines(arquivo);
-		
+
 		ListaAlunos listaAlunos = new ListaAlunos();
-		
-		for(int i = 0; i < nomes.size(); i++) {
+
+		for (int i = 0; i < nomes.size(); i++) {
 			String nome = nomes.get(i);
-			
+
 			Aluno aluno = new Aluno();
 			aluno.nome = nome;
-			
+
 			listaAlunos.adicionar(aluno);
 		}
-		
+
 		listaAlunos.ordenar();
-		
-		String mensagem = "Os alunos são: \n"; // Lembrando que o código "\n" é para indicar que a linha encerrou e a próxima parte da String será impressa na linha posterior.
-		
+
+		List<String> nomesOrdenados = new ArrayList<String>();
 		for (int i = 0; i < listaAlunos.tamanho(); i++) {
 			Aluno aluno = listaAlunos.obter(i);
-			mensagem += (i + 1) + ". " + aluno.nome + "\n";
+			nomesOrdenados.add(aluno.nome);
 		}
-		
-		enviarEmail("alexandre.afonso@algaworks.com", "Lista de alunos", mensagem);
-		
-		System.out.println("Enviado!");
-	}
-	
-	static void enviarEmail(String para, String assunto, String msg) throws EmailException {
-		Email email = new SimpleEmail();
-		
-		email.setHostName("smtp.gmail.com");
-		email.setSmtpPort(465);
-		email.setAuthenticator(new DefaultAuthenticator("cursologica.algaworks", "!@#$%qwert"));
-		email.setSSLOnConnect(true);
-		email.setFrom("Curso Lógica <cursologica.algaworks@gmail.com>");
-		email.setSubject(assunto);
-		email.setMsg(msg);
-		email.addTo(para);
-		
-		email.send();
+
+		Path novoArquivo = Paths.get("/tmp/nomes-ordenados.txt");
+		Files.write(novoArquivo, nomesOrdenados);
 	}
 }
